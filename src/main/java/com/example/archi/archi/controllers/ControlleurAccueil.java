1827dfb27594b395ca0a6cca8f81d1e9c41402b6
etud <etud@localhost>
2023-11-14 12:38:16 +0100

    setup for web interface

--- src/main/java/com/example/archi/archi/controllers/ControlleurAccueil.java
+++ src/main/java/com/example/archi/archi/controllers/ControlleurAccueil.java
@@ -1,13 +1,14 @@
 package com.example.archi.archi.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ControlleurAccueil {
-    @RequestMapping("/")
+    @GetMapping("/accueil")
     public String Accueil(){
-        return "Accueil";
+        return "welcome";
     }
 }
