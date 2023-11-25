70d219e4640e552f56ef351dd3a82926ecab1ca6
Kemrone <mcroain@gmail.com>
2023-11-25 21:26:33 +0100

    Setup connection salesforce

--- src/main/java/com/example/archi/archi/controllers/ControlleurAccueil.java
+++ src/main/java/com/example/archi/archi/controllers/ControlleurAccueil.java
@@ -2,13 +2,20 @@ package com.example.archi.archi.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+
+import com.example.archi.archi.businesslogic.salesforce.SalesforceConnection;
+
 
 @Controller
 public class ControlleurAccueil {
     @GetMapping("/accueil")
-    public String Accueil(){
+    public String Accueil() throws Exception{
+    	SalesforceConnection SC = new SalesforceConnection();
+    	SC.login();
+    	//SalesforceJSONHandler sjh = SalesforceJSONHandler.loadConfig("jsonFiles/SalesforceUser.json");
+    	System.out.println("End");
         return "welcome";
+        
     }
 }
