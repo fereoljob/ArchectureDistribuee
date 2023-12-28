dac6280101da1c44c43b20ade30424f92cfc9d2f
fereoljob <job.fereol@gmail.com>
2023-12-28 20:05:48 +0100

    update controller

--- src/main/java/com/example/archi/infra/controllers/CRMController.java
+++ src/main/java/com/example/archi/infra/controllers/CRMController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.archi.service.VirtualCRMService;
@@ -16,8 +17,8 @@ import com.example.archi.service.VirtualLeadDTO;
 
 
 @RestController
-@RequestMapping("/myTest")
-public class MarcController {
+@RequestMapping("/virtualcrm")
+public class CRMController {
     @Autowired
     private VirtualCRMService vcrm;
     @GetMapping
@@ -27,32 +28,13 @@ public class MarcController {
         System.out.println("End");
         return "Virtual Servers are Ready";
     }
-    
-    public String sendCommand(@RequestBody String command) {
-        if ("Hello Rest!".equals(command)) {
-            return "Hello CMD!";
-        } else {
-            return "Invalid command";
-        }
+    @GetMapping(value="/findLeads")
+    public List<VirtualLeadDTO> findLeads(@RequestParam double lowAnnualRevenue,@RequestParam double highAnnualRevenue,@RequestParam String state){
+        return vcrm.findLeads(lowAnnualRevenue, highAnnualRevenue, state);
     }
-    
-    @PostMapping(value = "/commandGate", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public List<VirtualLeadDTO> processData(@RequestBody Map<String, Object> dataMap) {
-    	
-    	String commandName = (String) dataMap.get("name");
-    	switch(commandName) {
-    		case "findLeads":
-    			 return vcrm.findLeads((double) dataMap.get("lowAnnualRevenue"),
-    					(double) dataMap.get("highAnnualRevenue"),
-    					(String) dataMap.get("state")) ;
-    		case "findLeadsByDate":
-    			return vcrm.findLeadsByDate((String) dataMap.get("startDate"),
-    					(String) dataMap.get("endDate"));
-    		default:
-    			break;
-    	}
-    	return null;
-    
-        
+    @GetMapping(value="/findLeadsByDate")
+    public List<VirtualLeadDTO> findLeadByDate(@RequestParam String startDate,@RequestParam String endDate){
+        return vcrm.findLeadsByDate(startDate, endDate);
     }
+       
 }
